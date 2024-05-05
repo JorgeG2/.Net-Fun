bc9ec139dc15343c2199a3a27f45fbaeb3d53daa
Jorge Gonzales <95510250+JorgeG2@users.noreply.github.com>
2024-05-04 23:49:26 -0400

    started to work on the project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _Net_Fun;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Press any key to exit.");
+    }
+}
